9d3f14abd5606e246a485a1a50f755d7403a5c3e
komilovx.org <xojiakbarkomilov@icloud.com>
2025-12-10 01:28:06 +0500

    Fix HTML head and Navbar import for Vercel deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./pages/navbar";
+import Navbar from "./pages/Navbar";
 import Home from "./components/home/Home";
 import Login from "./components/login/Login";
 
